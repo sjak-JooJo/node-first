e3b10221d907137be15a2556169b979bfadf1c0d
JooJo <sjak0101@gmail.com>
2022-09-30 05:37:34 +0100

    node and node express and nodemon setup

---
+++ _steps.js
@@ -0,0 +1,18 @@
+/* 
+*1. create a folder
+*2. open command line to that folder
+*3. npm init
+*4. npm install express
+*5. create index.js
+
+-------------------------------
+For Express/node server
+--------------------------------
+*1. Require Express
+*2. Create app variable
+*3. declare port
+*4. set app.get('/)
+*5. listen to port
+*6. node index.js
+*7. check your browser fot that port
+*/
---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+
+app.get('/', (req, res) =>{
+    res.send('from my personal over own smarty pant!!!');
+});
+
+const users = [
+    {id:1, name:'Sabana', email: 'sabana@gmail.com', phone: '012200202002'},
+    {id:2, name:'sabnur', email: 'sabnur@gmail.com', phone: '012200202002'},
+    {id:3, name:'Suchorita', email: 'Suchorita@gmail.com', phone: '012200202002'},
+    {id:4, name:'Suchonda', email: 'suchonda@gmail.com', phone: '012200202002'},
+    {id:5, name:'srabon', email: 'srabon@gmail.com', phone: '012200202002'},
+    {id:6, name:'srodda', email: 'srodda@gmail.com', phone: '012200202002'},
+    {id:7, name:'sonali', email: 'sonali@gmail.com', phone: '012200202002'},
+]
+
+app.get('/users', (req, res) => {
+    res.send(users);
+})
+
+
+app.get('/user/:id', (req, res) =>{
+    console.log(req.params);
+    const id = parseInt(req.params.id);
+    const user = users.find(u => u.id === id);
+
+    res.send (user);
+})
+
+app.listen(port, () =>{
+    console.log('Listening to port', port);
+})
+
+/* app.get() */
